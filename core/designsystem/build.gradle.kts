e491d38502ca8d0422122bb6cb79f951ca5dfbcc
Saeed <mr.ms.moradi@gmail.com>
2022-11-27 23:08:38 +0330

    Add design system module

---
+++ core/designsystem/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("fiki.android.library")
+    id("fiki.android.library.compose")
+    id("fiki.android.library.jacoco")
+}
+
+android {
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    namespace = "com.saeed.fiki.core.designsystem"
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    api(libs.androidx.compose.foundation)
+    api(libs.androidx.compose.foundation.layout)
+    api(libs.androidx.compose.material.iconsExtended)
+    api(libs.androidx.compose.material3)
+    debugApi(libs.androidx.compose.ui.tooling)
+    api(libs.androidx.compose.ui.tooling.preview)
+    api(libs.androidx.compose.ui.util)
+    api(libs.androidx.compose.runtime)
+}
